75c18b8d2db384bb89e95144a90028b94d09b92f
Yassine Najmi <yassine.najmi01@gmail.com>
2024-05-12 21:41:41 +0100

    added the gradient line between sections

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,24 +3,27 @@ import NavBar from "./NavBar";
 import SpaceButton from "./SpaceButton";
 export default function Header() {
   return (
-    <div className="header-container">
-      <NavBar />
-      <video autoPlay loop muted className="bg-video">
-        <source src={headerVideo} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <img className="saturn-img" src="src/assets/images/saturn.png" alt="" />
-      <header className="header">
-        <h1>
-          Welcome to My <span>Cosmic Voyage</span>
-        </h1>
-        <p>
-          Hey there, ready to embark on a cosmic adventure ? Join me as we
-          explore the vast universe, marvel at distant stars, and uncover its
-          mysteries together. Let's make cosmic memories!
-        </p>
-        <SpaceButton>Join Me</SpaceButton>
-      </header>
+    <div>
+      <div className="header-container">
+        <NavBar />
+        <video autoPlay loop muted className="bg-video">
+          <source src={headerVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <img className="saturn-img" src="src/assets/images/saturn.png" alt="" />
+        <header className="header">
+          <h1>
+            Welcome to My <span>Cosmic Voyage</span>
+          </h1>
+          <p>
+            Hey there, ready to embark on a cosmic adventure ? Join me as we
+            explore the vast universe, marvel at distant stars, and uncover its
+            mysteries together. Let's make cosmic memories!
+          </p>
+          <SpaceButton>Join Me</SpaceButton>
+        </header>
+      </div>
+      <div className="gradient"></div>
     </div>
   );
 }
--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,27 +1,30 @@
 import SpaceButton from "./SpaceButton.jsx";
 export default function Join() {
   return (
-    <section id="contact" className="join-section">
-      <img
-        className="astronaut-right-hand-img"
-        src="src/assets/images/astronaut-right-hand.png"
-        alt="astronaut right hand"
-      />
-      <div className="join-container">
-        <h2>Get in Touch</h2>
-        <p>
-          Have a question, suggestion, or just want to say hello? Feel free to
-          email me! Whether it's about black holes, distant galaxies, or your
-          love for all things cosmic, I'm here to chat.
-        </p>
+    <div>
+      <section id="contact" className="join-section">
+        <img
+          className="astronaut-right-hand-img"
+          src="src/assets/images/astronaut-right-hand.png"
+          alt="astronaut right hand"
+        />
+        <div className="join-container">
+          <h2>Get in Touch</h2>
+          <p>
+            Have a question, suggestion, or just want to say hello? Feel free to
+            email me! Whether it's about black holes, distant galaxies, or your
+            love for all things cosmic, I'm here to chat.
+          </p>
 
-        <SpaceButton email={"yassine.najmi01@gmail.com"}>
-          Get in Touch
-        </SpaceButton>
-        {/* <a href="_blank" type="button" className="join-btn">
+          <SpaceButton email={"yassine.najmi01@gmail.com"}>
+            Get in Touch
+          </SpaceButton>
+          {/* <a href="_blank" type="button" className="join-btn">
           Get in Touch
         </a> */}
-      </div>
-    </section>
+        </div>
+      </section>
+      <div className="gradient"></div>
+    </div>
   );
 }
